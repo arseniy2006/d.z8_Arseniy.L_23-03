1c13a944031cc539ff7dba0ec905d1a187ec94d8
arseniy2006 <59921629+arseniy2006@users.noreply.github.com>
2023-04-05 16:40:48 +0600

    Update app.js

--- dz8_Arseniy.L_23-03/app.js
+++ dz8_Arseniy.L_23-03/app.js
@@ -8,20 +8,4 @@ fetch('https://jsonplaceholder.typicode.com/photos')
         });
     })
     .catch(error => console.error(error))
-//
-// async function fetchPhotos() {
-//     try {
-//         const response = await fetch('https://jsonplaceholder.typicode.com/photos');
-//         const data = await response.json();
-//         data.forEach(photo => {
-//             const img = document.createElement('img');
-//             img.src = photo.url;
-//             document.body.appendChild(img);
-//         });
-//     } catch (error) {
-//         console.error(error);
-//     }
-// }
-//
-// fetchPhotos();
 
